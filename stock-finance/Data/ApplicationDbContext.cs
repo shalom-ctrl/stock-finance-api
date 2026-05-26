a1fdbcfa3f06031807e881d6c2d80ece17a577bb
Shalom Achikasim <shalomachims@gmail.com>
2026-05-26 12:02:07 +0100

    Add Models and EF Core Migration

---
+++ stock-finance/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using stock_finance.Models;
+
+namespace stock_finance.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> dbContextoptions)
+            : base(dbContextoptions)
+        {
+        }
+
+        public DbSet<Stock> Stocks { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+    }
+}
