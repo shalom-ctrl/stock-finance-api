8df50f228257c664f1b3a9cc5f36cc1416beb22c
Shalom Achikasim <shalomachims@gmail.com>
2026-05-28 16:26:28 +0100

    Add pagination to stock listings

--- stock-finance/Repository/StockRepository.cs
+++ stock-finance/Repository/StockRepository.cs
@@ -56,7 +56,9 @@ namespace stock_finance.Repository
                 }
             }
 
-            return await stocks.ToListAsync();
+            var skipNumber = (query.PageNumber - 1) * query.PageSize;
+
+            return await stocks.Skip(skipNumber).Take(query.PageSize).ToListAsync();
         }
 
         public async Task<Stock?> GetStockByIdAsync(int id)
